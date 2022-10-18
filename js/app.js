551649b657d6e6900dd9e32ff0755d736c5fc296
Eric Separovic <ericseparovic07@gmail.com>
2022-10-18 08:32:00 -0300

    Add validation to new task input and fix error in css

--- js/app.js
+++ js/app.js
@@ -1,41 +1,50 @@
 const newTaskInput = document.querySelector('#new-task');
 const tasksDiv = document.querySelector('#tasks-div')
 
+newTaskInput.addEventListener('keyup', createNewTask)
+tasksDiv.addEventListener('click', taskCompleted)
 
+// Craete a new task and save it on an array
 let tasks = []
-const createNewTask = (e) => {
+function createNewTask(e) {
     const task = {}
 
-
-    if (e.code === 'Enter') {
-        task.id = idGenerator()
-        task.description = newTaskInput.value
-        task.status = false //False = task not completed true task completed
-
-        tasks = [...tasks, task]
-
-        showTasks(tasks)
-        newTaskInput.value = ""
+    if (e.code !== 'Enter') {
+        return
     }
+
+    task.id = idGenerator()
+    task.description = newTaskInput.value
+    task.status = false //False = task not completed true task completed
+
+    if (task.description === "") {
+        // Show error
+        return alert('Input empty')
+    }
+    tasks = [...tasks, task]
+
+    showTasks(tasks)
+    newTaskInput.value = ""
 }
 
 
-newTaskInput.addEventListener('keyup', createNewTask)
 
 
-
+// id generator
 let id = 0;
 function idGenerator() {
     return id++;
 }
 
+
+// Show tasks in the DOM
 function showTasks(tasks) {
     let html = ''
     tasks.forEach(task => {
         const { id, description, status } = task;
         html += `
             <div class="items__task">
-                <label class="items__checkbox checkbox"></label>
+                <label id="checkbox" class="items__checkbox"></label>
                 <input value="${description}" class="items__input-task" type="text" name="" id="" placeholder="Create a new todo" />
                 <div class="items__delete"></div>
             </div>       
@@ -47,3 +56,35 @@ function showTasks(tasks) {
         tasksDiv.innerHTML = html;
     });
 }
+
+
+
+
+
+
+// mark task complete
+function taskCompleted(e) {
+
+
+    tasks.map(task)
+
+
+
+
+    if (e.target.id === 'checkbox' && !e.target.classList.contains('checkbox-active')) {
+        e.target.classList.add('checkbox-active')
+        e.target.parentElement.querySelector('.items__input-task').classList.add('items__input-task--active')
+        return
+    }
+
+
+    if (e.target.id === 'checkbox' && e.target.classList.contains('checkbox-active')) {
+
+        e.target.classList.remove('checkbox-active')
+        e.target.parentElement.querySelector('.items__input-task').classList.remove('items__input-task--active')
+        return
+    }
+
+
+}
+
