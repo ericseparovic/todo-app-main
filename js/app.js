3a8ec4080286b6cbe997a4905dfe2f151d320a57
Eric Separovic <ericseparovic07@gmail.com>
2022-10-18 08:54:01 -0300

    Add function that modifies the status of the task and modifies the style

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ function createNewTask(e) {
         // Show error
         return alert('Input empty')
     }
+
     tasks = [...tasks, task]
 
     showTasks(tasks)
@@ -42,8 +43,9 @@ function showTasks(tasks) {
     let html = ''
     tasks.forEach(task => {
         const { id, description, status } = task;
-        html += `
-            <div class="items__task">
+        if (!status) {
+            html += `
+            <div class="items__task" id="${id}">
                 <label id="checkbox" class="items__checkbox"></label>
                 <input value="${description}" class="items__input-task" type="text" name="" id="" placeholder="Create a new todo" />
                 <div class="items__delete"></div>
@@ -51,9 +53,21 @@ function showTasks(tasks) {
         
         `
 
+            tasksDiv.innerHTML = html;
 
+        } else {
+            html += `
+            <div class="items__task" id="${id}">
+                <label id="checkbox" class="items__checkbox checkbox-active"></label>
+                <input value="${description}" class="items__input-task items__input-task--active" type="text" name="" id="" placeholder="Create a new todo" />
+                <div class="items__delete"></div>
+            </div>       
+        
+            `
+        }
 
         tasksDiv.innerHTML = html;
+
     });
 }
 
@@ -66,23 +80,38 @@ function showTasks(tasks) {
 function taskCompleted(e) {
 
 
-    tasks.map(task)
-
-
+    let taskId = Number(e.target.parentElement.id)
 
 
     if (e.target.id === 'checkbox' && !e.target.classList.contains('checkbox-active')) {
-        e.target.classList.add('checkbox-active')
-        e.target.parentElement.querySelector('.items__input-task').classList.add('items__input-task--active')
-        return
+        // Modify task status
+        tasks = tasks.map(task => {
+            if (task.id === taskId) {
+                task.status = true
+                return task
+            } else {
+                return task
+            }
+        })
+
+        showTasks(tasks)
     }
 
 
     if (e.target.id === 'checkbox' && e.target.classList.contains('checkbox-active')) {
 
-        e.target.classList.remove('checkbox-active')
-        e.target.parentElement.querySelector('.items__input-task').classList.remove('items__input-task--active')
-        return
+        // Modify task status
+        tasks = tasks.map(task => {
+            if (task.id === taskId) {
+                task.status = false
+                return task
+            } else {
+                return task
+            }
+        })
+
+        showTasks(tasks)
+
     }
 
 
