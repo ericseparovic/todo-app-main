44612ed39837f39d67077df876044c64eb8588c5
Eric Separovic <ericseparovic07@gmail.com>
2022-10-21 12:21:12 -0300

    Add dark mode and light mode of tasks and background

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ const btnClearCompletedMobile = document.querySelector(
 );
 const countItemsDesktop = document.querySelector("#countItemsDesktop");
 const countItemsMobile = document.querySelector("#countItemsMobile");
+const btnTheme = document.querySelector("#btn-theme");
+const iconTheme = document.querySelector("#icon-theme");
+
 const btnFilter = document.querySelector("#btn-filter");
 let statusFilter = "all";
 
@@ -18,9 +21,11 @@ document.addEventListener("DOMContentLoaded", (e) => {
   btnClearCompletedDesktop.addEventListener("click", clearCompleted);
   btnClearCompletedMobile.addEventListener("click", clearCompleted);
   btnFilter.addEventListener("click", filter);
+  btnTheme.addEventListener("click", changeTheme);
 
   filter(e);
 });
+let theme = "dark";
 
 // Craete a new task and save it on an array
 let tasks = [];
@@ -69,11 +74,12 @@ function showTasks(tasks) {
 
   reverseTasks.forEach((task) => {
     const { id, description, status } = task;
+
     if (!status) {
       html += `
             <div class="items__task" id="${id}">
-                <label id="checkbox" class="items__checkbox"></label>
-                <input value="${description}" class="items__input-task" type="text" name="" id="" placeholder="Create a new todo" />
+                <label id="checkbox" class="items__checkbox items__checkbox--${theme}"></label>
+                <input value="${description}" class="items__input-task items__input-task--${theme}" type="text" name="" id="" placeholder="Create a new todo" />
                 <div id="btn-delete" class="items__delete"></div>
             </div>       
         
@@ -83,8 +89,8 @@ function showTasks(tasks) {
     } else {
       html += `
             <div class="items__task" id="${id}">
-                <label id="checkbox" class="items__checkbox checkbox-active"></label>
-                <input value="${description}" class="items__input-task items__input-task--active" type="text" name="" id="" placeholder="Create a new todo" />
+                <label id="checkbox" class="items__checkbox checkbox-active items__checkbox--${theme}"></label>
+                <input value="${description}" class="items__input-task items__input-task--active items__input-task--${theme}" type="text" name="" id="" placeholder="Create a new todo" />
                 <div id="btn-delete" class="items__delete"></div>
             </div>       
         
@@ -234,4 +240,91 @@ function colorSelectFilter(e) {
       btn.classList.remove("btn-filter-select--blue");
     }
   });
+}
+
+function changeTheme() {
+  //Change icon
+  changeIconTheme();
+
+  console.log(theme);
+  changeColorInputNewTask(theme);
+  changeColorItemsFooter(theme);
+  changeColorBody(theme);
+  showTasks(tasks);
+}
+
+function changeColorInputNewTask(theme) {
+  let inputnewtask = document.querySelector(".items__input-new-task");
+
+  if (theme === "dark") {
+    inputnewtask.classList.remove("items__input-new-task--light");
+    inputnewtask.classList.add("items__input-new-task--dark");
+  }
+
+  if (theme === "light") {
+    inputnewtask.classList.remove("items__input-new-task--dark");
+    inputnewtask.classList.add("items__input-new-task--light");
+  }
+}
+
+function changeColorItemsFooter(theme) {
+  let itemsFooter = document.querySelector(".items__footer");
+
+  if (theme === "dark") {
+    itemsFooter.classList.remove("items__footer--light");
+    itemsFooter.classList.add("items__footer--dark");
+  }
+
+  if (theme === "light") {
+    itemsFooter.classList.add("items__footer--light");
+    itemsFooter.classList.remove("items__footer--dark");
+  }
+}
+
+function changeColorItemsTasks(theme) {
+  let itemsBody = document.querySelector(".items__body");
+
+  if (itemsBody.childElementCount === 0) {
+    return;
+  }
+
+  let itemsTasks = document.querySelector(".items__input-task");
+  console.log(itemsTasks);
+
+  if (theme === "dark") {
+    itemsTasks.classList.remove("items__input-task--light");
+    itemsTasks.classList.add("items__input-task--dark");
+  }
+
+  if (theme === "light") {
+    itemsTasks.classList.add("items__input-task--light");
+    itemsTasks.classList.remove("items__input-task--dark");
+  }
+}
+
+function changeIconTheme() {
+  let url = iconTheme.baseURI;
+  let srcIcon = iconTheme.src;
+
+  if (srcIcon === `${url}images/icon-sun.svg`) {
+    iconTheme.src = `/images/icon-moon.svg`;
+    theme = "light";
+  } else {
+    iconTheme.src = `/images/icon-sun.svg`;
+    theme = "dark";
+  }
+}
+
+function changeColorBody(theme) {
+  let body = document.querySelector("body");
+
+  if (theme === "dark") {
+    body.classList.remove("bg--light");
+    body.classList.add("bg--dark");
+  }
+
+  if (theme === "light") {
+    body.classList.add("bg--light");
+    body.classList.remove("bg--dark");
+  }
 }
